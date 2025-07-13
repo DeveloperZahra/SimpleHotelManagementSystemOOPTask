58f922458a3a0bd0e4bc4f1f7219411d7a490f3a
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-13 15:42:06 +0400

    HotelUtils Class

---
+++ HotelUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelManagementSystemOOPTask
+{
+    //-------------------- HotelUtils Class ------------------------
+    // Static class with utility methods for the hotel
+    static class HotelUtils
+    {
+        // Prints a welcome message
+        public static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to our Hotel! We hope you enjoy your stay.");
+        }
+
+        // Checks if a room is available
+        public static bool IsRoomAvailable(ROOM room)
+        {
+            if (room.IsBooked)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
