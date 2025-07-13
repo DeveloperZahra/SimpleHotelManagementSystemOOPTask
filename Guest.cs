1046d808be062b9b66edd7712e618871865edb54
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-13 10:42:56 +0400

    Create a class called Guest

---
+++ Guest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelManagementSystemOOPTask
+{
+    // Class definition for Guest
+    internal class Guest
+    {
+        
+        //class Auto-property (get , set)
+        public string Name { get; set; }
+        public int NationalID { get; set; }
+   
+
+    // Constructor... Allows creating a new Guest object and specifying the name and national number directly.
+    public Guest(string name, string nationalId)
+        {
+            Name = name;
+            NationalID = NationalID;
+        }
+
+
+
+
+    }
+}
